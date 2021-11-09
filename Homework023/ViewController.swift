7253d7a7f1331ac4da2ef0d1f0a28d128fff702b
Poga Huang <pgh86812@gmail.com>
2021-11-09 08:53:36 +0800

    Initial Commit

---
+++ Homework023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework023
+//
+//  Created by 黃柏嘉 on 2021/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
